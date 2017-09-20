a92652017863107ba62a1ad3d937854a9828b1ca
Tom_s <tomasmateo98@gmail.com>
2017-09-20 11:57:23 +0200

    Ejercicio empezado

---
+++ Programacion/PrimerTrimestre/Boletin2Ejercicio12.java
@@ -0,0 +1,35 @@
+/* Nombre: Boletin2Ejercicio12
+ * 
+ * Análisis:
+ * 		 Escribe un algoritmo para calcular el cuadrado y el cubo
+ * 		 de un número introducido por teclado. 
+ * 
+ * 		 -Entrada: numero entero o decimal
+ * 		 -Salida: cubo y cuadrado del número
+ * 
+ * 	PG:
+ * 		Inicio
+ * 			Leer y validar el numero
+ * 			Calcular el cuadrado
+ * 			Calcular el cubo
+ * 			Pintar resultado
+ * 		Fin
+ * 
+ * 
+ */
+
+
+public class Boletin2Ejercicio12 {
+	
+	public static void main (String args[]) {
+		
+Inicio
+	Leer y validar el numero
+	Calcular el cuadrado
+	Calcular el cubo
+	Pintar resultado
+Fin
+		
+	}
+}
+
