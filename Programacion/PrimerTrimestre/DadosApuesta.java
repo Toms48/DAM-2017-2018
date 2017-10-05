9e4724c34278513f609146bc2b9cedaa9e3df249
Tom_s <tomasmateo98@gmail.com>
2017-10-05 11:12:28 +0200

    DadoApuesta

---
+++ Programacion/PrimerTrimestre/DadosApuesta.java
@@ -0,0 +1,30 @@
+/*
+ * Nombre: DadosApuesta
+ * 
+ * Comentario: Se escoge un numero entre 2 y 12, se apuesta una cantidad de dinero y se comparan los resultados
+ * 
+ * Análisis:
+ * 		-Entra:
+ * 		-Salida:
+ * 
+ * PG:
+ * 		Inicio
+ * 			Hacer
+ * 				Escoger dinero incial
+ * 				Generar numero aleatorio para la opcion del Pc
+ * 				Escoger un numero (Opcion del Jugador)
+ * 				Apuesta Pc
+ * 				Apuesta Jugador
+ * 			Mientras
+ * 		Fin
+ * 
+ */
+
+
+public class DadosApuesta {
+	
+	public static void main (String[] args) {
+		
+	}
+}
+
