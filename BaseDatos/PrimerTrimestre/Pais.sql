877cc67340b2dc412c06102e111de191b6ba5443
Tom_s <tomasmateo98@gmail.com>
2017-11-07 12:44:41 +0100

    SQL - Pais

--- BaseDatos/PrimerTrimestre/Pais.sql
+++ BaseDatos/PrimerTrimestre/Pais.sql
@@ -4,39 +4,39 @@ GO
 USE Pais
 GO
 
-CREATE TABLE comunidad(
+CREATE TABLE Comunidad(
 	Nombre varchar(20) NOT NULL
 		CONSTRAINT PK_comunidad PRIMARY KEY,
 	Superficie int NOT NULL,
 	Poblacion int NOT NULL,
-	/*Nombre_localidad varchar(30) NOT NULL
-		CONSTRAINT FK_comunidad_localidad FOREIGN KEY
+	Nombre_localidad varchar(30) NOT NULL
+		/*CONSTRAINT FK_comunidad_localidad FOREIGN KEY
 		REFERENCES localidad(Nombre)*/
 )
 
-CREATE TABLE provincia(
+CREATE TABLE Provincia(
 	CP int NOT NULL
-		CONSTRAINT PK_provincia PRIMARY KEY,
+		CONSTRAINT PK_Provincia PRIMARY KEY,
 	Nombre varchar(20) NOT NULL,
 	Superficie int NOT NULL,
 	Poblacion int NOT NULL,
-	/*Nombre_localidad varchar(30) NOT NULL
-		CONSTRAINT FK_provincia_localidad FOREIGN KEY
-		REFERENCES localidad(Nombre),
+	Nombre_localidad varchar(30) NOT NULL,
+		/*CONSTRAINT FK_provincia_localidad FOREIGN KEY
+		REFERENCES localidad(Nombre),*/
 	Nombre_comunidad varchar(30) NOT NULL
-		CONSTRAINT FK_provincia_comunidad FOREIGN KEY
+		/*CONSTRAINT FK_provincia_comunidad FOREIGN KEY
 		REFERENCES comunidad(Nombre)*/
 )
 
-CREATE TABLE localidad(
+CREATE TABLE Localidad(
 	Nombre varchar(30) NOT NULL
-		CONSTRAINT PK_localidad PRIMARY KEY,
+		CONSTRAINT PK_Localidad PRIMARY KEY,
 	Poblacion int NOT NULL,
-	/*CP_provincia int NOT NULL
-		CONSTRAINT FK_localidad_provincia FOREIGN KEY
+	CP_provincia int NOT NULL
+		/*CONSTRAINT FK_localidad_provincia FOREIGN KEY
 		REFERENCES provincia(CP)*/
 )
 
-ALTER TABLE comunidad
-	ADD CONSTRAINT FK_comunidad_localidad FOREIGN KEY (Nombre_localidad)
-		REFERENCES localidad(Nombre)
+ALTER TABLE Comunidad ADD
+	CONSTRAINT FK_comunidad_localidad FOREIGN KEY (Nombre_localidad)
+	REFERENCES localidad(Nombre)
