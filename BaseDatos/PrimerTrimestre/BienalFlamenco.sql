f76d15a28d2e0b68082d9405c5d9d9473151bdc3
Tom_s <tomasmateo98@gmail.com>
2017-11-14 12:32:53 +0100

    Bienal Flamenco con anotaciones de la pizarra

--- BaseDatos/PrimerTrimestre/BienalFlamenco.sql
+++ BaseDatos/PrimerTrimestre/BienalFlamenco.sql
@@ -4,6 +4,10 @@ GO
 USE BienalFlamenco
 GO
 
+--Mirar los NOT NULL
+--Repasar el LIKE para el DNI
+--
+
 CREATE TABLE Empresa(
 	CIF int NOT NULL
 		CONSTRAINT PK_Empresa PRIMARY KEY, 
@@ -21,7 +25,7 @@ CREATE TABLE Trabajador(
 	CIF_Empresa int NOT NULL
 )
 
---ALTER para las FK de las tablas
+--ALTER para la FK
 ALTER TABLE Trabajador ADD
 	CONSTRAINT FK_CIF_Empresa FOREIGN KEY (CIF_Empresa) REFERENCES Empresa(CIF)
 	ON DELETE NO ACTION
@@ -33,25 +37,25 @@ CREATE TABLE Espectaculo(
 	DNI_Trabajador char(10) NOT NULL
 )
 
---ALTER para las FK de las tablas
+--ALTER para la FK
 ALTER TABLE Espectaculo ADD
-	CONSTRAINT FK_Trabajador FOREIGN KEY (DNI_Trabajador) REFERENCES Trabajador(DNI)
+	CONSTRAINT FK_DNITrabajador FOREIGN KEY (DNI_Trabajador) REFERENCES Trabajador(DNI)
 	ON DELETE NO ACTION
 	ON UPDATE NO ACTION
 
-CREATE TABLE Artista(  --FK DNI_Trabajador
+CREATE TABLE Artista(
 	DNI char(10) NOT NULL
 		CONSTRAINT PK_Artista PRIMARY KEY,
 	Nombre varchar(20) NOT NULL,
 	Apellido varchar(20) NOT NULL,
 	Direccion varchar (50) NULL,
 	TipoTrabajo varchar(50) NOT NULL,
-	DNI_Representante varchar(10) NOT NULL
+	DNI_Representante char(10) NOT NULL
 )
 
---ALTER para las FK de las tablas
-ALTER TABLE Espectaculo ADD
-	CONSTRAINT FK_ FOREIGN KEY (DNI_Trabajador) REFERENCES Trabajador(DNI)
+--ALTER para la FK
+ALTER TABLE Artista ADD
+	CONSTRAINT FK_Representante FOREIGN KEY (DNI_Representante) REFERENCES Representante(DNI)
 	ON DELETE NO ACTION
 	ON UPDATE NO ACTION
 
@@ -80,15 +84,29 @@ CREATE TABLE Localidad(
 	Dia varchar(20) NOT NULL,
 	Hora time NOT NULL,
 	Fila int NOT NULL,
-	Asiento int NOT NULL
+	Asiento int NOT NULL,
+	ID_Zona int NOT NULL
 )
+
+--ALTER para la FK
+ALTER TABLE Localidad ADD
+	CONSTRAINT FK_Zona FOREIGN KEY (ID_Zona) REFERENCES Zona(ID)
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
 
 CREATE TABLE Zona(
 	ID int NOT NULL
 		CONSTRAINT PK_Zona PRIMARY KEY,
 	Fila int NOT NULL,
-	Asiento int NOT NULL
+	Asiento int NOT NULL,
+	ID_Espacio int NOT NULL
 )
+
+--ALTER para la FK
+ALTER TABLE Zona ADD
+	CONSTRAINT FK_Espacio FOREIGN KEY (ID_Espacio) REFERENCES Espacio(ID)
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
 
 CREATE TABLE Espacio(
 	ID int NOT NULL
@@ -99,6 +117,46 @@ CREATE TABLE Espacio(
 	Aforo int NOT NULL
 )
 
+CREATE TABLE EspacioEspectaculo(
+	ID_Espacio int NOT NULL
+		CONSTRAINT FK_EspacioEspectaculo_Espacio FOREIGN KEY (ID_Espacio) REFERENCES Espacio(ID),
+	ID_Espectaculo int NOT NULL
+		CONSTRAINT FK_EspacioEspectaculo_Espectaculo FOREIGN KEY (ID_Espectaculo) REFERENCES Espectaculo(ID),
+	CONSTRAINT PK_EspacioEspectaculo PRIMARY KEY (ID_Espacio, ID_Espectaculo)
+)
+
+CREATE TABLE LocalidadFuncion(
+	ID_Localidad int NOT NULL
+		CONSTRAINT FK_LocalidadFuncion_Localidad FOREIGN KEY (ID_Localidad) REFERENCES Localidad(ID),
+	ID_Funcion int NOT NULL
+		CONSTRAINT FK_LocalidadFuncion_Funcion FOREIGN KEY (ID_Funcion) REFERENCES Funcion(ID),
+	CONSTRAINT PK_LocalidadFuncion PRIMARY KEY (ID_Localidad, ID_Funcion)
+)
+
+CREATE TABLE ArtistaEspectaculo(
+	DNI_Artista char(10) NOT NULL
+		CONSTRAINT FK_ArtistaEspectaculo_Artista FOREIGN KEY (DNI_Artista) REFERENCES Artista(DNI),
+	ID_Espectaculo int NOT NULL
+		CONSTRAINT FK_ArtistaEspectaculo_Espectaculo FOREIGN KEY (ID_Espectaculo) REFERENCES Espectaculo(ID),
+	CONSTRAINT PK_ArtistaEspectaculo PRIMARY KEY (DNI_Artista, ID_Espectaculo)
+)
+
+CREATE TABLE TrabajadorEspectaculo(
+	DNI_Trabajador char(10) NOT NULL
+		CONSTRAINT FK_TrabajadorEspectaculo_Trabajador FOREIGN KEY (DNI_Trabajador) REFERENCES Trabajador(DNI),
+	ID_Espectaculo int NOT NULL
+		CONSTRAINT FK_TrabajadorEspectaculo_Espectaculo FOREIGN KEY (ID_Espectaculo) REFERENCES Espectaculo(ID),
+	CONSTRAINT PK_TrabajadorEspectaculo PRIMARY KEY (DNI_Trabajador, ID_Espectaculo)
+)
+
+ALTER TABLE Empresa ADD
+	CONSTRAINT CK_CIF CHECK(CIF > 0)
+
+ALTER TABLE Trabajador ADD
+	CONSTRAINT CK_TipoTrabajo CHECK(TipoTrabajo IN ('técnico','organizador','relaciones','seguridad','transportista'))
+
+ALTER TABLE Artista ADD
+	CONSTRAINT CK_TipoTrabajoTrabajador CHECK(TipoTrabajo IN ('cantores','tocadores','bailaores','músicos de acompañamiento','actores'))
 
 
 
