95486d7d5f0d1c5dd0e955db81fa6fe6df6505b0
Tom_s <tomasmateo98@gmail.com>
2017-11-29 13:12:32 +0100

    Programación - Conductor y funcionalidades de DadosApuesta con Rafa

--- Programacion/PrimerTrimestre/Subprogramas/DadosApuesta con Rafa/cDadosApuestaModulado.java
+++ Programacion/PrimerTrimestre/Subprogramas/DadosApuesta con Rafa/cDadosApuestaModulado.java
@@ -1,11 +1,12 @@
-import 
-
 public class cDadosApuestaModulado {
 	
 	public static void main (String[] args) {
 		
-		System.out.println(fDadosApuestaModulado());
+		//System.out.println(fDadosApuestaModulado.GenerarSumarDados());
+		
+		//System.out.println(fDadosApuestaModulado.CalcularDiferenciaApuesta(5,6));
+		//System.out.println(fDadosApuestaModulado.CalcularDiferenciaApuesta(80,3));
+		System.out.println(fDadosApuestaModulado.CalcularDiferenciaApuesta(80,3));
 		
 	}
 }
-
--- Programacion/PrimerTrimestre/Subprogramas/DadosApuesta con Rafa/fDadosApuestaModulado.java
+++ Programacion/PrimerTrimestre/Subprogramas/DadosApuesta con Rafa/fDadosApuestaModulado.java
@@ -7,6 +7,23 @@ import java.util.Random;
 
 public class fDadosApuestaModulado {
 	
+/**************************************************************************	
+MT
+	Nec: No hay
+	Dev: numero
+	Nec/Dev: No hay
+	Restriccion: No hay
+
+Interfaz
+	Comentario: Subprograma que genera dos numeros aleatorios simulando dos dados y suma las dos cifras de estos 
+	Cabecera: numero GenerarSumarDados()
+	Precondiciones: No hay
+	Entrada: No hay
+	Salida: numero
+	E/S: No hay
+	Postcondiciones: El numero debe de estar entre comprendido 2 y 12
+**************************************************************************/	
+	
 	public static int GenerarSumarDados () {
 		
 		int dado1 = 0;
@@ -25,5 +42,41 @@ public class fDadosApuestaModulado {
 		return sumaDados;
 		
 	}
+	
+/**************************************************************************	
+MT
+	Nec:
+		- numero para el numero por el que se apuesta
+		- numero para la suma de sus dados
+	Dev: numero
+	Nec/Dev: No hay
+	Restriccion: No hay
+
+Interfaz
+	Comentario: Subprograma que calcula la diferencia entre el numero que se cree que saldrá y el numero que sale realmente
+	Cabecera: numero CalcularDiferenciaApuesta(numero numeroElegido, numero sumaDados)
+	Precondiciones: No hay
+	Entrada:
+		- numero para el numero por el que se apuesta
+		- numero para la suma de sus dados
+	Salida: numero
+	E/S: No hay
+	Postcondiciones: el numero siempre sera positivo
+**************************************************************************/	
+	
+	public static int CalcularDiferenciaApuesta(int numeroElegido, int sumaDados){
+		
+		int diferencia = 0;
+		
+		diferencia = numeroElegido - sumaDados;
+					
+		if(diferencia < 0){
+			diferencia = diferencia * -1;
+		}
+		
+		return diferencia;
+		
+	}
+	
 }
 
