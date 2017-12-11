1edd2f592a1ffd8574e00d6dab1b6aaab3b82aba
Tom_s <tomasmateo98@gmail.com>
2017-12-11 23:09:18 +0100

    BD - Laboratorio Qímico
    Hasta el ejercicio 7

--- BaseDatos/PrimerTrimestre/LaboratorioQuimico.sql
+++ BaseDatos/PrimerTrimestre/LaboratorioQuimico.sql
@@ -50,8 +50,36 @@ CREATE TABLE LQ_Moleculas(
 /*5. Añade dos nuevas columnas a la tabla LQ_Elementos: numero_atomico (entero corto, sin nulos) y masa_atomica (tres cifras enteras y cinco decimales, admite nulos).
 El número atómico no debe admitir valores inferiores a uno ni superiores a 300 y será clave alternativa.*/
 
-ALTER TABLE LQ_Elementos DROP
+ALTER TABLE LQ_Elementos ADD
 	numero_atomico smallint NOT NULL
 
 ALTER TABLE LQ_Elementos ADD
 	masa_atomica decimal(8,5) NULL
+
+/*6. Añade una nueva columna a LQ_Moleculas llamada codigo. Será de tipo entero, identidad y defínela como clave primaria.*/
+
+ALTER TABLE LQ_Moleculas ADD
+	codigo int NOT NULL IDENTITY(1,1)
+		CONSTRAINT PK_LQ_Moleculas PRIMARY KEY
+
+/*7. Crea una nueva tabla que relacione LQ_Elementos con LQ_Moleculas, llámala LQ_MoleculasElementos.
+Añade una columna numero que indique el subíndice del elemento en la molécula (entero corto sin nulos, valor mínimo 1).
+Crea las restricciones de clave primaria y ajena que sean necesarias.*/
+
+CREATE TABLE LQ_MoleculasElementos(
+	subindiceElementoMolecula tinyint NOT NULL
+		CONSTRAINT CK_LQ_MoleculasElementos CHECK (subindiceElementoMolecula >= 1),
+	LQ_Elementos_LQ_MoleculasElementos char(2) NOT NULL,
+	LQ_Moleculas_LQ_MoleculasElementos int NOT NULL,
+	CONSTRAINT PK_LQ_MoleculasElementos PRIMARY KEY (LQ_Elementos_LQ_MoleculasElementos, LQ_Moleculas_LQ_MoleculasElementos)
+)
+
+ALTER TABLE LQ_MoleculasElementos ADD
+	CONSTRAINT LQ_MoleculasElementos_LQ_Elementos FOREIGN KEY (LQ_Elementos_LQ_MoleculasElementos) REFERENCES LQ_Elementos(simbolo)
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
+
+ALTER TABLE LQ_MoleculasElementos ADD
+	CONSTRAINT LQ_MoleculasElementos_LQ_Moleculas FOREIGN KEY (LQ_Moleculas_LQ_MoleculasElementos) REFERENCES LQ_Moleculas(codigo)
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION
