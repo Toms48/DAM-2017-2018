50bb0f473ec28bd9ddf9da3d0b427659b2f34d8b
Tom_s <tomasmateo98@gmail.com>
2017-12-19 14:16:22 +0100

    BD - Boletín 7.2
    Completado hasta el ejercicio 4.

---
+++ BaseDatos/Boletin7punto2.sql
@@ -0,0 +1,31 @@
+/*1. Título, precio y notas de los libros (titles) que tratan de cocina, ordenados de mayor a menor precio.*/
+SELECT * FROM titles
+SELECT title,price,notes FROM titles
+	WHERE type IN ('mod_cook','trad_cook')
+	--WHERE type LIKE '%cook%'
+	ORDER BY price DESC
+
+/*2. ID, descripción y nivel máximo y mínimo de los puestos de trabajo (jobs) que pueden tener un nivel 110.*/
+SELECT * FROM jobs
+	WHERE min_lvl >= 110
+
+/*3. Título, ID y tema de los libros que contengan la palabra "and” en las notas.*/
+SELECT * FROM titles
+SELECT title_id, title, type, notes FROM titles
+	WHERE notes LIKE '%and%'
+
+/*4. Nombre y ciudad de las editoriales (publishers) de los Estados Unidos que no estén en California ni en Texas.*/
+SELECT * FROM publishers
+SELECT pub_name,city FROM publishers
+	WHERE country = 'USA' AND state NOT IN ('CA', 'TX')
+
+/*5. Título, precio, ID de los libros que traten sobre psicología o negocios y cuesten entre diez y 20 dólares.*/
+SELECT * FROM titles
+SELECT title, price, title_id FROM titles
+	WHERE type IN ('psychology', '')
+
+/*6. Nombre completo (nombre y apellido) y dirección completa de todos los autores que no viven en California ni en Oregón.*/
+
+/*7. Nombre completo y dirección completa de todos los autores cuyo apellido empieza por D, G o S.*/
+
+/*8. ID, nivel y nombre completo de todos los empleados con un nivel inferior a 100, ordenado alfabéticamente.*/
