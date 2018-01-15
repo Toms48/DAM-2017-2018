5d2235b076099d8c51edabd8570b7544bda0391a
Tom_s <tomasmateo98@gmail.com>
2018-01-15 14:30:24 +0100

    BD - Boletín 8.3

--- BaseDatos/Boletin8punto3.sql
+++ BaseDatos/Boletin8punto3.sql
@@ -3,22 +3,38 @@ USE AdventureWorks2012
 				/**Consultas sencillas**/
 
 /*1.Nombre del producto, código y precio, ordenado de mayor a menor precio*/
+SELECT * FROM Production.Product
 
+SELECT Name,ProductNumber, ListPrice FROM Production.Product
+	ORDER BY ListPrice DESC
 
 /*2.Número de direcciones de cada Estado/Provincia*/
+SELECT * FROM Person.Address
 
+SELECT COUNT(AddressID) AS [Número de direcciones], StateProvinceID FROM Person.Address
+	GROUP BY StateProvinceID
 
 /*3.Nombre del producto, código, número, tamaño y peso de los productos que estaban a la venta durante todo el mes de septiembre de 2002.
 No queremos que aparezcan aquellos cuyo peso sea superior a 2000.*/
+SELECT * FROM Production.Product
 
+SET DATEFORMAT ymd
+
+SELECT Name, ProductID, ProductNumber, Size, [Weight] FROM Production.Product
+	WHERE SellStartDate BETWEEN '2002-09-01' AND '2002-09-30'
 
 /*4.Margen de beneficio de cada producto (Precio de venta menos el coste), y porcentaje que supone respecto del precio de venta.*/
+SELECT * FROM Production.Product
+
+SELECT (ListPrice - StandardCost) AS [Margen de beneficio] FROM Production.Product
 
 
 				/**Consultas de dificultad media**/
 
 /*5.Número de productos de cada categoría*/
+SELECT * FROM Production.ProductSubcategory
 
+SELECT COUNT(ProductID) FROM Production.Product
 
 /*6.Igual a la anterior, pero considerando las categorías generales (categorías de categorías).*/
 
