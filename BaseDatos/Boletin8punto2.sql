879eafdf05ae9cb1175a4d6cc493db4c4ab5da62
Tom_s <tomasmateo98@gmail.com>
2018-01-15 14:30:08 +0100

    BD - Boletín 8.2

--- BaseDatos/Boletin8punto2.sql
+++ BaseDatos/Boletin8punto2.sql
@@ -14,16 +14,30 @@ SELECT COUNT(au_id), city, state FROM authors
 	ORDER BY state
 
 /*3. Nombre, apellidos, nivel y antigüedad en la empresa de los empleados con un nivel entre 100 y 150.*/
+SELECT * FROM employee
 
+SELECT fname, lname, job_lvl, Year(Current_Timestamp - hire_date)-1900 AS [Años de antigüedad] FROM employee
+	WHERE job_lvl BETWEEN 100 AND 150
 
 /*4. Número de editoriales en cada país. Incluye el país.*/
+SELECT * FROM publishers
 
+SELECT country, COUNT(pub_id) AS [Número de editoriales] FROM publishers
+	GROUP BY country
 
 /*5. Número de unidades vendidas de cada libro en cada año (title_id, unidades y año).*/
+SELECT * FROM sales
 
+SELECT title_id, qty, YEAR(ord_date) AS [Año de pedido] FROM sales
 
 /*6. Número de autores que han escrito cada libro (title_id y numero de autores).*/
+SELECT * FROM titleauthor
 
+SELECT title_id, au_ord FROM titleauthor
 
 /*7. ID, Titulo, tipo y precio de los libros cuyo adelanto inicial (advance) tenga un valor superior a $7.000, ordenado por tipo y título*/
+SELECT * FROM titles
 
+SELECT title_id, title, [type], price FROM titles
+	WHERE advance > 7000
+	ORDER BY [type], title
