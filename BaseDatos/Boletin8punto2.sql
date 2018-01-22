3b9ce2b4a34a1655b9fa69ad11eefccb73447fa3
Tom_s <tomasmateo98@gmail.com>
2018-01-22 14:30:33 +0100

    BD - Boletín 8.2 y 9.1

--- BaseDatos/Boletin8punto2.sql
+++ BaseDatos/Boletin8punto2.sql
@@ -28,12 +28,14 @@ SELECT country, COUNT(pub_id) AS [Número de editoriales] FROM publishers
 /*5. Número de unidades vendidas de cada libro en cada año (title_id, unidades y año).*/
 SELECT * FROM sales
 
-SELECT title_id, qty, YEAR(ord_date) AS [Año de pedido] FROM sales
+SELECT title_id, SUM(qty) AS [Número de unidades vendidas], YEAR(ord_date) AS [Año de pedido] FROM sales
+	GROUP BY title_id, YEAR(ord_date)
 
 /*6. Número de autores que han escrito cada libro (title_id y numero de autores).*/
 SELECT * FROM titleauthor
 
-SELECT title_id, au_ord FROM titleauthor
+SELECT title_id, COUNT(au_ord) AS [Número de autores por libro] FROM titleauthor
+	group by title_id
 
 /*7. ID, Titulo, tipo y precio de los libros cuyo adelanto inicial (advance) tenga un valor superior a $7.000, ordenado por tipo y título*/
 SELECT * FROM titles
