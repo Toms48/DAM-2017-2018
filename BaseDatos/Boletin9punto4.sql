568b5cb7f7b5faa5fdfd4b3661eddc1dcef87220
Tom_s <tomasmateo98@gmail.com>
2018-02-08 23:28:38 +0100

    BD - Boletín 9.4

--- BaseDatos/Boletin9punto4.sql
+++ BaseDatos/Boletin9punto4.sql
@@ -77,14 +77,47 @@ SELECT NumeroMascotas.[Número de mascotas], NumeroMascotas.Especie, Enfermedades
 	ON Enfermedades.Nombre = NumeroMascotas.Nombre
 
 /*7.Queremos saber cuál es la enfermedad más común en cada especie. Incluye cuantos casos se han producido*/
+SELECT * FROM BI_Enfermedades
+SELECT * FROM BI_Mascotas_Enfermedades
+SELECT * FROM BI_Mascotas
 
 
 /*8.Duración media, en días, de cada enfermedad, desde que se detecta hasta que se cura. Incluye solo los casos en que el animal se haya curado.
 Se entiende que una mascota se ha curado si tiene fecha de curación y está viva o su fecha de fallecimiento es posterior a la fecha de curación.*/
+SELECT * FROM BI_Mascotas
+SELECT * FROM BI_Mascotas_Enfermedades
+
 
 /*9.Número de veces que ha acudido a consulta cada cliente con alguna de sus mascotas. Incluye nombre y apellidos del cliente.*/
+SELECT * FROM BI_Clientes
+SELECT * FROM BI_Mascotas
+SELECT * FROM BI_Visitas
+
+SELECT c.Nombre, COUNT(v.IDVisita) AS [Número de visitas]
+	FROM BI_Clientes AS c
+	INNER JOIN BI_Mascotas AS m
+	ON c.Codigo = m.CodigoPropietario
+	INNER JOIN BI_Visitas AS v
+	ON m.Codigo = v.Mascota
+		GROUP BY c.Nombre
 
 /*10.Número de visitas a las que ha acudido cada mascota, fecha de su primera y de su última visita*/
+SELECT * FROM BI_Mascotas
+SELECT * FROM BI_Visitas
+
+SELECT COUNT(v.IDVisita), m.Alias, v.Fecha
+	FROM BI_Mascotas AS m
+	INNER JOIN BI_Visitas AS v
+	ON m.Codigo = v.Mascota
+		GROUP BY m.Alias, v.Fecha
+		ORDER BY v.Fecha
+
+SELECT m.Alias, v.Fecha
+	FROM BI_Mascotas AS m
+	INNER JOIN BI_Visitas AS v
+	ON m.Codigo = v.Mascota
+		GROUP BY m.Alias, v.Fecha
+		ORDER BY m.Alias, v.Fecha
 
 /*11.Incremento (o disminución) de peso que ha experimentado cada mascota entre cada dos consultas sucesivas.
 Incluye nombre de la mascota, especie, fecha de las dos consultas sucesivas e incremento o disminución de peso.*/
