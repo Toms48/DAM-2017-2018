349870f821ca6f33f9136ae3c92ca30dfcbb9e8e
Tom_s <tomasmateo98@gmail.com>
2018-04-24 12:40:20 +0200

    Programación - Cosas

--- Programacion/Tercer Trimestre/Boletin11A/src/Ejercicio5.java
+++ Programacion/Tercer Trimestre/Boletin11A/src/Ejercicio5.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+
 /*********************************************************************************************************************
  * Nombre: Ejercicio5
  *
@@ -11,7 +16,7 @@
 
 //PG
 //Inicio
-    //Preguntar y leer ruta del archivo
+    //Preguntar, leer y validar ruta del archivo
     //Contar numero de palabras del fichero
     //Contar numero de parrafos del fichero
     //Contar el numero de caracteres del fichero
@@ -23,5 +28,48 @@ public class Ejercicio5 {
 
     public static void main (String[] args) {
 
+        String rutaString;
+        boolean existe;
+
+        Scanner teclado = new Scanner(System.in);
+
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        //Inicio
+            //Preguntar, leer y validar ruta del archivo
+            do{
+                System.out.print("Introduzca una ruta existente: ");
+
+                rutaString = teclado.nextLine();
+
+                File ruta = new File(rutaString);
+
+                existe = ruta.exists();
+
+                if(existe==false){
+                    System.out.println("El fichero no existe o la ruta esta mal escrita, por favor introduzca vuelva a escribir la ruta");
+                    System.out.println(" ");
+                }
+
+            }
+            while(existe != true);
+
+            //Contar numero de palabras del fichero
+
+
+            //Contar numero de parrafos del fichero
+
+            try{
+
+            }
+            catch{
+
+            }
+
+            //Contar el numero de caracteres del fichero
+            //Calcular la media de caracteres por palabra
+            //Calcular la media de palabras por parrafo
+        //Fin
     }
 }
