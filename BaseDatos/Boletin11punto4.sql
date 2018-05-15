1cf9308a77d5f92733838d211f35561ab7425ef3
Tom_s <tomasmateo98@gmail.com>
2018-05-15 12:37:22 +0200

    BD - Boletin11.4

--- BaseDatos/Boletin11punto4.sql
+++ BaseDatos/Boletin11punto4.sql
@@ -2,27 +2,32 @@
 Ejercicio 1
 Escribe un procedimiento EliminarUsuario que reciba como parámetro el DNI de un usuario, le coloque un NULL en la columna Sex y borre todas las reservas futuras de ese usuario.
 Ten en cuenta que si alguna de esas reservas tiene asociado un alquiler de material habrá que borrarlo también.
+
+He tenido en cuenta que la fecha actual es 2014-01-01
 */
 SELECT * FROM Usuarios
 SELECT * FROM Reservas
 SELECT * FROM ReservasMateriales
 
+--SELECT para ver las reservas que tiene cada usuario
 SELECT u.DNI AS [ID usuario], r.Codigo AS [Codigo reserva], r.Fecha_Hora
 	FROM Usuarios AS u
 	INNER JOIN Reservas AS r
 	ON u.ID = r.ID_Usuario
 		ORDER BY u.DNI
 
+--SELECT para ver la reserva de material de cada usuario
 SELECT u.ID, r.Codigo, rm.IDMaterial, r.Fecha_Hora
 	FROM Usuarios AS u
 	LEFT JOIN Reservas AS r
 	ON u.ID = r.ID_Usuario
 	LEFT JOIN ReservasMateriales AS rm
 	ON r.Codigo = rm.CodigoReserva
-		WHERE r.Fecha_Hora > '2014-01-01'
+		--WHERE r.Fecha_Hora > '2014-01-01'
 
 GO
-ALTER PROCEDURE EliminarUsuario @DNI char(12)
+CREATE PROCEDURE EliminarUsuario
+	@DNI char(12)
 AS
 	BEGIN
 		UPDATE Usuarios
@@ -54,15 +59,138 @@ COMMIT
 
 /*
 Ejercicio 2
-Escribe un procedimiento que reciba como parámetros el código de una instalación y una fecha/hora (SmallDateTime) y devuelva en otro parámetro de salida el ID del usuario que la tenía alquilada si en ese momento la instalación estaba ocupada. Si estaba libre, devolverá un NULL.
+Escribe un procedimiento que reciba como parámetros el código de una instalación y una fecha/hora (SmallDateTime)
+y devuelva en otro parámetro de salida el ID del usuario que la tenía alquilada si en ese momento la instalación estaba ocupada.
+Si estaba libre, devolverá un NULL.
 */
+SELECT * FROM Usuarios
+SELECT * FROM Reservas
+SELECT * FROM Instalaciones
+
+SELECT u.ID, r.Codigo AS [CodigoReserva], i.Codigo AS [CodigoInstalacion], r.Fecha_Hora
+	FROM Usuarios AS u
+	INNER JOIN Reservas AS r
+	ON u.ID = r.ID_Usuario
+	INNER JOIN Instalaciones AS i
+	ON r.Cod_Instalacion = i.Codigo
+		--WHERE @CodigoInstalacion = i.Codigo AND @Fecha = r.Fecha_Hora
+		ORDER BY u.ID
+
+GO
+CREATE PROCEDURE InstalacionAlquilada
+	 @CodigoInstalacion int
+	,@Fecha smalldatetime
+	,@IDUsuario char(12) OUTPUT
+AS
+	BEGIN
+		IF EXISTS (SELECT u.ID, r.Codigo AS [CodigoReserva], i.Codigo AS [CodigoInstalacion], r.Fecha_Hora
+						FROM Usuarios AS u
+						INNER JOIN Reservas AS r
+						ON u.ID = r.ID_Usuario
+						INNER JOIN Instalaciones AS i
+						ON r.Cod_Instalacion = i.Codigo
+							WHERE @CodigoInstalacion = i.Codigo AND @Fecha = r.Fecha_Hora)
+			BEGIN
+				SET @IDUsuario = (SELECT u.ID
+									FROM Usuarios AS u
+									INNER JOIN Reservas AS r
+									ON u.ID = r.ID_Usuario
+									INNER JOIN Instalaciones AS i
+									ON r.Cod_Instalacion = i.Codigo
+										WHERE @CodigoInstalacion = i.Codigo AND @Fecha = r.Fecha_Hora)
+			END
+		ELSE
+			BEGIN
+				SET @IDUsuario = NULL
+			END
+	END
+GO
+
+BEGIN TRANSACTION
+	DECLARE @IDUsuario char(12)
+	EXECUTE InstalacionAlquilada 1, '2018-12-12 15:00:00', @IDUsuario OUTPUT
+	PRINT ISNULL(@IDUsuario, 'NULL')
+ROLLBACK
+COMMIT
 
 /*
 Ejercicio 3
-Escribe un procedimiento que reciba como parámetros el código de una instalación y dos fechas (DATE) y devuelva en otro parámetro de salida el número de horas que esa instalación ha estado alquilada entre esas dos fechas, ambas incluidas. Si se omite la segunda fecha, se tomará la actual con GETDATE().
+Escribe un procedimiento que reciba como parámetros el código de una instalación y dos fechas (DATE)
+y devuelva en otro parámetro de salida el número de horas que esa instalación ha estado alquilada entre esas dos fechas, ambas incluidas.
+Si se omite la segunda fecha, se tomará la actual con GETDATE().
 
-Devuelve con return códigos de error si el código de la instalación es erróneo  o si la fecha de inicio es posterior a la de fin.
+Devuelve con return códigos de error si el código de la instalación es erróneo o si la fecha de inicio es posterior a la de fin.
 */
+SELECT * FROM Usuarios
+SELECT * FROM Reservas
+SELECT * FROM Instalaciones
+
+SELECT u.ID, r.Codigo AS [CodigoReserva], i.Codigo AS [CodigoInstalacion], r.Fecha_Hora
+	FROM Usuarios AS u
+	INNER JOIN Reservas AS r
+	ON u.ID = r.ID_Usuario
+	INNER JOIN Instalaciones AS i
+	ON r.Cod_Instalacion = i.Codigo
+		ORDER BY u.ID
+
+SELECT i.Codigo AS [CodigoInstalacion], COUNT(r.Fecha_Hora) AS [Cantidad de horas alquilada]
+	FROM Usuarios AS u
+	INNER JOIN Reservas AS r
+	ON u.ID = r.ID_Usuario
+	INNER JOIN Instalaciones AS i
+	ON r.Cod_Instalacion = i.Codigo
+		WHERE i.Codigo = 1 AND r.Fecha_Hora BETWEEN '2008-01-01' AND '2014-01-01'
+		GROUP BY i.Codigo	
+
+GO
+ALTER PROCEDURE InstalacionHorasAlquiladas
+	 @CodigoInstalacion int
+	,@HorasAlquiladas int OUTPUT
+	,@FechaInicio date
+	,@FechaFin date = NULL
+AS
+	BEGIN
+		DECLARE @codigoError int = 0
+		
+		IF(@FechaFin IS NULL)
+			BEGIN
+				SET @FechaFin = GETDATE()
+			END
+
+		IF EXISTS (SELECT Codigo FROM Instalaciones)
+			BEGIN
+				IF (@FechaInicio < @FechaFin)
+					BEGIN
+						SET @HorasAlquiladas = (SELECT COUNT(r.Fecha_Hora) AS [Cantidad de horas alquilada]
+													FROM Usuarios AS u
+													INNER JOIN Reservas AS r
+													ON u.ID = r.ID_Usuario
+													INNER JOIN Instalaciones AS i
+													ON r.Cod_Instalacion = i.Codigo
+														WHERE i.Codigo = @CodigoInstalacion AND r.Fecha_Hora BETWEEN @FechaInicio AND @FechaFin)
+					END
+				ELSE
+					BEGIN
+						SET @codigoError = 2
+					END
+			END
+		ELSE
+			BEGIN
+				SET @codigoError = 1
+			END
+		RETURN @codigoError
+	END
+GO
+
+BEGIN TRANSACTION
+	DECLARE @HorasAlquiladas int
+	DECLARE @CodigoError int
+
+	EXECUTE @CodigoError = InstalacionHorasAlquiladas 1, @HorasAlquiladas OUTPUT, '2008-01-01'
+	PRINT CONCAT('Cantidad de horas alquilada: ', @HorasAlquiladas)
+	PRINT CONCAT('Código de error: ', @CodigoError)
+ROLLBACK
+COMMIT
 
 /*
 Ejercicio 4
